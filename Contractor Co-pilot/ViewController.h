84f7dcb3997fae2a34fe65f6a7a7eec52ad50667
Jon Smith <Wolfwood@Wolfwood.local>
2015-08-17 08:14:58 -0400

    Initial Commit

---
+++ Contractor Co-pilot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contractor Co-pilot
+//
+//  Created by Jon Smith on 8/17/15.
+//  Copyright (c) 2015 Jon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
